921af7697b9d8494b4a133af741c20d58ea354c2
Pegasus221q <48793220+Pegasus221q@users.noreply.github.com>
2020-02-21 10:21:08 +0100

    almost did it

--- user_insert.php
+++ user_insert.php
@@ -8,14 +8,27 @@ $email = mysqli_real_escape_string($link, $_POST['email']);
 $pass1 = mysqli_real_escape_string($link, $_POST['pass1']);
 $pass2 = mysqli_real_escape_string($link, $_POST['pass2']);
 
-//preverim. če je uporabnik pravilno izpolnil obrazec
-if (!empty($first_name) && !empty($last_name) && !empty($email)
-        && !empty($pass1) && ($pass1==$pass2)) {
-    //vse ok
+
+
+        
+        
+        
+        if (!empty($first_name) && !empty($last_name) && !empty($email)
+        && !empty($pass1) && ($pass1==$pass2))
+        {
+
+            $sql = "SELECT * FROM users where email='$email'";
+        $resul=mysqli_query($link,$sql);
+        $num = mysqli_num_rows($resul);
+        
+        if ($num !=0){ //If there is already such username...
+
+            die("There is user with that email.");  // ...kill the script!
+        } else {
+                //vse ok
     $pass = $salt.$pass1;
     $pass = sha1($pass);
-    
-    $query = "INSERT INTO users(first_name,last_name,email,pass) "
+                $query = "INSERT INTO users(first_name,last_name,email,pass) "
             . "VALUES ('$first_name','$last_name','$email','$pass')";
     mysqli_query($link, $query);
 
@@ -23,9 +36,12 @@ if (!empty($first_name) && !empty($last_name) && !empty($email)
     header('Location:  index.php');
     
 }
+        }
 else {
     //preusmeritev nazaj
     header("Location: registration.php");
 }
+        
+    
 
 ?>
