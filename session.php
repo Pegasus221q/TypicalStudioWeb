03715b3451b93a0f55c8eec2d01a6c0c150d1d11
Pegasus221q <48793220+Pegasus221q@users.noreply.github.com>
2020-04-06 11:22:44 +0200

    Update session.php

--- session.php
+++ session.php
@@ -1,12 +1,4 @@
 <?php
     session_start();
-    
-    $allowed = [''];
-    
-    
-    if (!isset($_SESSION['user_id']) && 
-            !in_array($_SERVER['REQUEST_URI'], $allowed)) {
-
-    }
 
 ?>
